b7dd9efb51efc3db22d02386017ad22b132fd0fc
olivierdalang <olivier.dalang@gmail.com>
2026-07-15 16:34:43 +0200

    improve dist

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -1,1 +1,4 @@
-window.STAC_BROWSER_CONFIG = {};
+window.STAC_BROWSER_CONFIG = {
+  // uncomment this to preconfigure the catalog (both relative and absolute urls are supported)
+  // catalogUrl: './catalog.json'
+};
