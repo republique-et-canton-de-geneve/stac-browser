2ae5faef70f2fa5d938b3b4068dd6eefc534b748
olivierdalang <olivier.dalang@gmail.com>
2026-07-29 15:27:10 +0200

    dist: better (portable) config out of the box

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -1,4 +1,11 @@
 window.STAC_BROWSER_CONFIG = {
-  // uncomment this to preconfigure the catalog (both relative and absolute urls are supported)
-  // catalogUrl: './catalog.json'
+  // portable configuration that resolves paths dynamically
+  historyMode: "history",
+  pathPrefix: new URL(location.href).pathname,
+
+  // uncomment this to preconfigure the catalog
+  // catalogUrl: new URL("./catalog.json", location.href).href, // relative url
+  // catalogUrl: "https://example.com/catalog.json", // absolute url
+
+  // see https://github.com/radiantearth/stac-browser/blob/main/docs/options.md for more options
 };
